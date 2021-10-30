6586f8d8e3765991f59a10c1991b8bb8cfdd094e
Bassam <bassambj@gmail.com>
2021-10-30 13:32:26 -0300

    Create Color.kt
    Colors resources in Resources root.

---
+++ Jetpack/Compose-Workshop/Color.kt
@@ -0,0 +1,11 @@
+package com.bassam.composebasics.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val Green700 = Color(0xFF00796B)
+val SkyBlue300 = Color(0xFF64B5F6)
+val Red300 = Color(0xFFE57373)
