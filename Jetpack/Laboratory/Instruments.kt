0d3b0a20553f714740b54fc539e0532f611a13e9
Bassam <bassambj@gmail.com>
2021-11-03 07:36:51 -0300

    Create Instruments.kt
    In project.

---
+++ Jetpack/Laboratory/Instruments.kt
@@ -0,0 +1,17 @@
+object Balance
+object Burner
+object CombustionSpoon
+object Dropper
+object Funnel
+object Glass
+object GlassBalloon
+object GlassPlates
+object GlassWatch
+object GraduateCylinder
+object Microscope
+object PipeSupport
+object Pipette
+object Spatula
+object TestTube
+object Thermometer
+object Tweezers
