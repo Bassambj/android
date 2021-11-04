18edae0d30da940b81e52b978cde559d3a3cfa03
Bassam <bassambj@gmail.com>
2021-11-03 22:16:20 -0300

    Update Instruments.kt
    Making some instruments functional.

--- Jetpack/Laboratory/Instruments.kt
+++ Jetpack/Laboratory/Instruments.kt
@@ -1,17 +1,62 @@
-object Balance
-object Burner
-object CombustionSpoon
-object Dropper
-object Funnel
-object Glass
-object GlassBalloon
-object GlassPlates
-object GlassWatch
-object GraduateCylinder
-object Microscope
-object PipeSupport
-object Pipette
-object Spatula
-object TestTube
-object Thermometer
-object Tweezers
+package laboratory
+
+//Objetos singleton/Singleton objects
+
+object Balance{
+    //Expressões lambda/lambda expressions
+    var tara: Double = 0.00
+    var pressure: Double = 0.00
+    //hypothetical: 1 press = 1kg
+    var result: Double = 0.00
+
+    fun toWeight(tara: Double,press: Double){
+        //hypothetical calculation:
+        this.result = (this.pressure*1)-this.tara
+    }
+}
+
+object Burner{
+    //Lambdas finais/final lambdas
+    var burning: Boolean = false
+    fun burn(start: Boolean){
+        this.burning = start
+
+    }
+}
+
+object CombustionSpoon {
+    var burningSpoon: Boolean = false
+    fun burnSpoon(start: Boolean) {
+        this.burningSpoon = start
+    }
+}
+
+
+object Dropper {
+    var liquid: Double = 0.0
+
+    fun cleanDropper(){
+        this.liquid = 0.0
+    }
+
+    fun getLiquid(drop: Double) {
+        this.liquid = this.liquid + drop
+    }
+
+    fun dropLiquid(drop:Double){
+        this.liquid = this.liquid -drop
+    }
+
+    fun dropStatus(): Double {
+        return this.liquid
+    }
+
+}
+
+
+object Microscope{
+    var zoom: Double = 1.0
+    fun zooming(zoomFactor: Double){
+        this.zoom = zoomFactor
+    }
+}
